2b95cd5dbb2097d2cb3a76a88b898923793fea3e
Cesar <cesar.j.pardo.barrientos@gmail.com>
2024-11-06 18:32:32 -0400

    backend init

---
+++ src/main/java/com/movil/boliviaXplore/BoliviaXploreApplication.java
@@ -0,0 +1,13 @@
+package com.movil.boliviaXplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoliviaXploreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoliviaXploreApplication.class, args);
+	}
+
+}
