38a3e66dffaa3baa5754decda0243fa09983c10c
Cesar <cesar.j.pardo.barrientos@gmail.com>
2024-12-15 03:40:15 -0400

    test to save a event without the images 5

--- src/main/java/com/movil/boliviaXplore/controllers/EventController.java
+++ src/main/java/com/movil/boliviaXplore/controllers/EventController.java
@@ -83,6 +83,17 @@ public class EventController {
         }
     }
 
+    @PostMapping("/registerevent")
+    public ResponseEntity<Event> createEvent2(@RequestBody Event event) {
+        System.out.println("entra al servidor");
+        try{
+            Event savedEvent = eventServiceImplement.saveEvent(event);
+            return new ResponseEntity<>(savedEvent, HttpStatus.OK);
+        } catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
     /*@PostMapping("/register")
     public ResponseEntity<Event> createEvent(@RequestPart("event") Event event, @RequestPart("imagenes") List<MultipartFile> multipartFile) {
         System.out.println("entra al servidor");
